f1373a4c2c8b1f2011de7cea138b84dd81572c64
andresovichh <ahenderson.holberton@gmail.com>
2022-04-18 10:45:33 -0300

    forwardslash was missing

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Used to create a file
-file { 'tmp/school':
-  ensure  => file,
+file { '/tmp/school':
+  ensure  => 'file',
   mode    => '0744',
-  owner   => www-data,
-  group   => www-data,
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet',
 }
