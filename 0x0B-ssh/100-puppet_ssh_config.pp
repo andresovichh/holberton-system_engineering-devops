31156a73363935339a6d1cfc4cdc38bae4b46657
andresovichh <ahenderson.holberton@gmail.com>
2022-04-18 17:07:00 -0300

    should work

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+# a file to use private key and no password
+
+file { '/Users/andreshenderson/.ssh/config':
+ensure => present,
+}
+file_line { 'connect using key':
+  path    => '/Users/andreshenderson/.ssh/config',
+  line    => 'IdentityFile ~/.ssh/shchool',
+  replace => true,
+}
+file_line { 'do not request password':
+  path    => '/Users/andreshenderson/.ssh/config',
+  line    => 'PasswordAuthentication no',
+  replace => true,
+}
