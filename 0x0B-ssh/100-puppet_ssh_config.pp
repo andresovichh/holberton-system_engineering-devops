d1df9a94ef7d9fd244100e9b560ddba00c28762f
andresovichh <ahenderson.holberton@gmail.com>
2022-04-19 09:47:57 -0300

    test 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,12 +2,12 @@
 
 file { '/Users/andreshenderson/.ssh/config':
 ensure => present,
-}
+}->
 file_line { 'connect using key':
   path    => '/Users/andreshenderson/.ssh/config',
   line    => 'IdentityFile ~/.ssh/shchool',
   replace => true,
-}
+}->
 file_line { 'do not request password':
   path    => '/Users/andreshenderson/.ssh/config',
   line    => 'PasswordAuthentication no',
