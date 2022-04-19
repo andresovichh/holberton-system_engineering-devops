91f0ef036ad3b216dd95c1bcf8ecc74f64ace374
andresovichh <ahenderson.holberton@gmail.com>
2022-04-19 09:51:36 -0300

    changes 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,12 +2,12 @@
 # a file to use private key and no password
 
 file_line { 'connect using key':
-  path    => '/Users/andreshenderson/.ssh/config',
-  line    => '    IdentityFile ~/.ssh/shchool',
+  path    => '/etc/ssh/.ssh/config',
+  line    => 'IdentityFile ~/.ssh/shchool',
   replace => true,
 }->
 file_line { 'do not request password':
-  path    => '/Users/andreshenderson/.ssh/config',
-  line    => '    PasswordAuthentication no',
+  path    => '/etc/ssh/.ssh/config',
+  line    => 'PasswordAuthentication no',
   replace => true,
 }
