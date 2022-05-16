86a2e5064a449f10e6a367fb9a8012d9ea7cfbb5
andresovichh <ahenderson.holberton@gmail.com>
2022-05-16 17:43:09 -0300

    test 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
 """ This is a module that gathers data from an API """
 
+import json
 import requests
 from sys import argv
-import json
+
 
 if __name__ == '__main__':
     url = 'https://jsonplaceholder.typicode.com/users/'
-    r = requests.get("{}/{}".format(url, argv[1]))
-    name = r.json().get('name')
-    r = json.loads(r.text)
+    rq = requests.get("{}/{}".format(url, argv[1]))
+    name = rq.json().get('name')
+    r = json.loads(rq.text)
     rj = json.dumps(r)
 
     req = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos"
                        .format(argv[1]))
-    todos = json.loads(req.text)
+    todos = req.json()
 
-    task = [task for task in todos]
-    final = {argv[1]: []}
-
-    for task in task:
-        tasks_dict = {"task": task.get('title'),
-                      "completed": task.get('completed'),
-                      "username": name
-                      }
-        final[argv[1]].append(tasks_dict)
     with open("{}.json".format(argv[1]), "w") as f:
+        tasks = []
+
+        for task in todos:
+            tasks.append({"task": task.get('title'),
+                         "completed": task.get('completed'),
+                          "username": rq.json().get('username')
+                          })
+        final = {"{}".format(argv[1]): tasks}
         json.dump(final, f)
