f3a998db87f707b7ce48e73bfb6cde29b51a93a7
andresovichh <ahenderson.holberton@gmail.com>
2022-05-16 16:37:46 -0300

    test 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -12,20 +12,20 @@ from sys import argv
 if __name__ == '__main__':
     """ adding some documentation 
     to this dfile"""
+    if len(argv[1]) > 1:
+        url = 'https://jsonplaceholder.typicode.com/users/'
+        r = requests.get("{}/{}".format(url, argv[1]))
+        name = r.json().get('name')
 
-    url = 'https://jsonplaceholder.typicode.com/users/'
-    r = requests.get("{}/{}".format(url, argv[1]))
-    name = r.json().get('name')
-
-    req = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos"
-                       .format(argv[1]))
-    todos = json.loads(req.text)
-    tasks = len(todos)
-    # print(tasks)
-    if name is not None:
-        with open("{}.csv".format(argv[1]), "w", newline="") as f:
-            writer = csv.writer(f, quoting=csv.QUOTE_ALL)
-            for task in todos:
-                writer.writerow([argv[1], r.json().get('username'),
-                                task.get('completed'),
-                                task.get('title')])
+        req = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos"
+                        .format(argv[1]))
+        todos = json.loads(req.text)
+        tasks = len(todos)
+        # print(tasks)
+        if name is not None:
+            with open("{}.csv".format(argv[1]), "w", newline="") as f:
+                writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+                for task in todos:
+                    writer.writerow([argv[1], r.json().get('username'),
+                                    task.get('completed'),
+                                    task.get('title')])
