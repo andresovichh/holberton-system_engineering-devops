bc94f8de9552b30c9d1cf41a635ff8ad9860934a
andresovichh <ahenderson.holberton@gmail.com>
2022-05-16 16:36:48 -0300

    test 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" This is a module that gathers data from an API
+"""
+This is a module that gathers data from an API
 and exports it to a CSV file """
 
 import csv
@@ -9,7 +10,8 @@ import requests
 from sys import argv
 
 if __name__ == '__main__':
-    """ adding some documentation to this dfile"""
+    """ adding some documentation 
+    to this dfile"""
 
     url = 'https://jsonplaceholder.typicode.com/users/'
     r = requests.get("{}/{}".format(url, argv[1]))
@@ -20,9 +22,10 @@ if __name__ == '__main__':
     todos = json.loads(req.text)
     tasks = len(todos)
     # print(tasks)
-    with open("{}.csv".format(argv[1]), "w", newline="") as f:
-        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
-        for task in todos:
-            writer.writerow([argv[1], r.json().get('username'),
-                             task.get('completed'),
-                             task.get('title')])
+    if name is not None:
+        with open("{}.csv".format(argv[1]), "w", newline="") as f:
+            writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+            for task in todos:
+                writer.writerow([argv[1], r.json().get('username'),
+                                task.get('completed'),
+                                task.get('title')])
