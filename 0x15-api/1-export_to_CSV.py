29e6f724122ee1f756f7e2cf7a14b8f15a4f3054
andresovichh <ahenderson.holberton@gmail.com>
2022-05-16 16:47:49 -0300

    test 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,31 +1,25 @@
 #!/usr/bin/python3
-"""
-This is a module that gathers data from an API
-and exports it to a CSV file """
+""" This is a module that gathers data from an API """
 
-import csv
-import json
-import pandas as pd
 import requests
 from sys import argv
+import json
+import csv
+import pandas as pd
 
 if __name__ == '__main__':
-    """ adding some documentation 
-    to this dfile"""
-    if len(argv[1]) > 1:
-        url = 'https://jsonplaceholder.typicode.com/users/'
-        r = requests.get("{}/{}".format(url, argv[1]))
-        name = r.json().get('name')
+    url = 'https://jsonplaceholder.typicode.com/users/'
+    r = requests.get("{}/{}".format(url, argv[1]))
+    name = r.json().get('name')
 
-        req = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos"
-                        .format(argv[1]))
-        todos = json.loads(req.text)
-        tasks = len(todos)
-        # print(tasks)
-        if name is not None:
-            with open("{}.csv".format(argv[1]), "w", newline="") as f:
-                writer = csv.writer(f, quoting=csv.QUOTE_ALL)
-                for task in todos:
-                    writer.writerow([argv[1], r.json().get('username'),
-                                    task.get('completed'),
-                                    task.get('title')])
+    req = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos"
+                       .format(argv[1]))
+    todos = json.loads(req.text)
+    tasks = len(todos)
+    # print(tasks)
+    with open("{}.csv".format(argv[1]), "w", newline="") as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for task in todos:
+            writer.writerow([argv[1], r.json().get('username'),
+                            task.get('completed'),
+                            task.get('title')])
