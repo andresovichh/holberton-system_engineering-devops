92ec9d82600d826971b029ad30671d9347908c8e
andresovichh <ahenderson.holberton@gmail.com>
2022-05-16 17:57:05 -0300

    test 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -7,20 +7,20 @@ from sys import argv
 
 
 if __name__ == '__main__':
-    url = 'https://jsonplaceholder.typicode.com/users/'
-    rq = requests.get("{}{}".format(url, argv[1]))
 
-    req = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos"
-                       .format(argv[1]))
-    todos = json.loads(req.text)
-    user = json.loads(rq.text)
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])
+    r = requests.get(url).json()
+    t = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
+                     .format(argv[1]))
+    t = t.json()
+
 
     with open("{}.json".format(argv[1]), "w") as f:
         tasks = []
 
-        for task in todos:
+        for task in t:
             tasks.append({"task": task.get('title'),
                          "completed": task.get('completed'),
-                        "username": user.get('username')})
+                        "username": r.get('username')})
         final = {"{}".format(argv[1]): tasks}
         json.dump(final, f)
