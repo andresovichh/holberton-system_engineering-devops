4be2247ef23af4e3eed2d034b10228f74b6a07fe
andresovichh <ahenderson.holberton@gmail.com>
2022-05-16 17:18:43 -0300

    test 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -13,15 +13,15 @@ if __name__ == '__main__':
     username = r.json().get('username')
 
     if username is not None:
-        todos = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos"
-                           .format(argv[1])).json()
-    # todos = json.loads(req.text)
+        t = requests.get("https://jsonplaceholder.typicode.com/users/{}/t"
+                         .format(argv[1])).json()
+    # t = json.loads(req.text)
     # print(tasks)
     with open("{}.csv".format(argv[1]), "w", newline="") as f:
         writer = csv.writer(f, quoting=csv.QUOTE_ALL)
-        for task in todos:
+        for task in t:
             writer.writerow([argv[1],
                             username,
                             task.get('completed'),
                             task.get('title')])
-    print(len(todos))
+    print(len(t))
