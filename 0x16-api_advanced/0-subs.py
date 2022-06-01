7d86db22c299e0889da960009ced37e3721dc823
andresh <ahenderson.holberton@gmail.com>
2022-06-01 09:58:50 -0300

    first commit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def number_of_subscribers(subreddit):
+    """
+    Returns the number of subscribers of a subreddit.
+    """
+    import requests
+    from pprint import pprint
+
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        # pprint(data)
+        return data['data']['subscribers']
+    else:
+        return 0
