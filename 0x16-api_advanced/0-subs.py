39479ff72eeed3bcccdafa4f451a6717fd4f07aa
andresh <ahenderson.holberton@gmail.com>
2022-06-01 10:01:21 -0300

    sytle fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+"""this is a module that gets the number of
+subscribers of a subreddit"""
+
+import requests
+from pprint import pprint
+
 
 def number_of_subscribers(subreddit):
     """
     Returns the number of subscribers of a subreddit.
     """
-    import requests
-    from pprint import pprint
-
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers)
